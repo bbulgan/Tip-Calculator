58ff7030e8823d8d3330ad6d3792951ca44debdc
Bilguun Bulgan <bbulgan@Bilguuns-MBP.attlocal.net>
2017-12-31 00:53:11 -0600

    minimal working tip calculator

--- Tippy/ViewController.swift
+++ Tippy/ViewController.swift
@@ -10,6 +10,12 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +26,22 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func onTap(_ sender: Any) {
+        view.endEditing(true)
+    }
+    
+    
+    @IBAction func calculateTip(_ sender: Any) {
+        let percentages = [0.18, 0.2, 0.25]
 
+        let bill = Double(billField.text!) ?? 0
+        //let tip = bill * 0.2
+        let tip = bill * percentages[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+    }
 }
 
